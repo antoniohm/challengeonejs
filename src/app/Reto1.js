1c45faed7eade9d4d486851e5f78ce451c21fa3b
Antonio Hernandez <antoniohm.ing@gmail.com>
2022-10-13 11:35:19 -0500

    correcciones

--- src/app/Reto1.js
+++ src/app/Reto1.js
@@ -1,11 +1,19 @@
 let scanf = require('scanf');
-
-console.log("Digita la edad de la persona")
-
+console.log("Digita la edad de la persona");
 let edad = scanf('%d');
-if (edad >= 18) {
-    console.log("La persona puede conducir")
-}
-else {
-    console.log("La persona no puede conducir")
-}
+
+while (isNaN(edad)){
+
+    console.log("Ingrese un número")
+    console.log("Digita la edad de la persona");
+    edad = scanf('%d');
+    
+};
+if (edad >= 18)
+        console.log("La persona puede conducir");
+    else
+        console.log("La persona no puede conducir");        
+   
+    
+
+
